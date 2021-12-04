12a9a6a02a9816e692ecb9d6e3270379cf69286e
Joakim Stenberg <joakim.stenberg88@gmail.com>
2021-12-04 10:54:57 +0100

    adding describing test to day 1

--- day1/index.test.js
+++ day1/index.test.js
@@ -3,12 +3,12 @@ const fileReader = require('../fileReader.js');
 
 const input = fileReader('./day1/test.txt');
 
-
-test('Solution A', () => {
-    expect(A(input)).toBe(7);
-});
-
-
-test('Solution B', () => {
-    expect(B(input)).toBe(5);
+describe('Day 1 Tests', () => {
+    test('Solution A', () => {
+        expect(A(input)).toBe(7);
+    });
+    
+    test('Solution B', () => {
+        expect(B(input)).toBe(5);
+    });
 });
