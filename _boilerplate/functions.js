bcce146ea2e23f4435efa245ada189829daf0f86
Joakim Stenberg <joakim.stenberg88@gmail.com>
2021-12-04 10:54:17 +0100

    adding boilerplate

---
+++ _boilerplate/functions.js
@@ -0,0 +1,9 @@
+const A = (input) => {
+    return 0;
+}
+
+const B = (input) => {
+    return 0;
+}
+
+module.exports = { A, B };
---
+++ _boilerplate/index.js
@@ -0,0 +1,9 @@
+const { A, B } = require('./functions.js');
+const fileReader = require('../fileReader.js');
+
+const input = fileReader('./day#/input.txt');
+
+console.log('----- Running Solution A -----');
+console.log(A(input));
+console.log('----- Running Solution B -----');
+console.log(B(input));
